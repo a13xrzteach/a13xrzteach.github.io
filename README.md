59d42780a6a706cc11084e46255a7b46610e3050
Michael Skyba <michaelskyba@net-c.com>
2023-09-13 12:21:58 -0400

    Implement main cycling functionality

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # a13xrzteach.github.io
 CsClub
+
+## Slide Cycling
+You can cycle through images at https://a13xrzteach.github.io/cycle.html.
+
+To modify the cycle, add your images to the
+[cycle_images/ folder](https://github.com/a13xrzteach/a13xrzteach.github.io/tree/main/cycle_images).
+
+Then, modify
+[cycle_config.js](https://github.com/a13xrzteach/a13xrzteach.github.io/blob/main/cycle_config.js).
---
+++ cycle_config.js
@@ -0,0 +1,17 @@
+/*
+The list of images to go through. These should all be in the
+cycle_images/ folder of the GitHub repo.
+Also, make sure they are using the same aspect ratio as your monitor
+if you want them to look right.
+*/
+const cycleImages = [
+	"sg-1.png",
+	"sg-2.png",
+	"sg-3.png",
+	"sg-4.png",
+	"sg-5.png",
+	"sg-6.png"
+]
+
+// How long each image should be shown (milliseconds)
+const imgDuration = 1000
