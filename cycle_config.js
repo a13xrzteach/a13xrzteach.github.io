371549be43e399e59f641f5f13bdc16d2c6ce762
Michael Skyba <michaelskyba@net-c.com>
2023-09-21 13:22:37 -0400

    Add QR 1

--- cycle_config.js
+++ cycle_config.js
@@ -5,12 +5,13 @@ Also, make sure they are using the same aspect ratio as your monitor
 if you want them to look right.
 */
 const cycleImages = [
+	"qr-1.png",
 	"sg-1.png",
 	"sg-2.png",
 	"sg-3.png",
 	"sg-4.png",
 	"sg-5.png",
-	"sg-6.png"
+	"sg-6.png",
 ]
 
 // How long each image should be shown (milliseconds)
