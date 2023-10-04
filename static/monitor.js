7bf35815db4b78d1a0af51d461f107b2dc6684ac
Michael Skyba <michaelskyba@net-c.com>
2023-10-04 10:41:56 -0400

    Switch to YouTube embed API for iframe logic

--- static/monitor.js
+++ static/monitor.js
@@ -24,16 +24,40 @@ const cycleMode = () => {
 	}, imgDuration)
 }
 
-const ytMode = () => {
-	const iframe = document.getElementById("iframe")
-	iframe.style.display = "block"
+let player
+function onYouTubePlayerAPIReady() {
+	player = new YT.Player("yt-player", {
+		videoId: ytID,
+		playerVars: {
+			autoplay: 1,
 
-	// playlist has to be set to the ID as well for looping to work
-	iframe.src = `https://www.youtube.com/embed/${ytID}?playlist=${ytID}&autoplay=1&mute=1&loop=1&controls=0`
+			// The video has to be explicitly muted for autoplaying to work
+			mute: 1,
 
+			controls: 0,
+			loop: 1,
+
+			// playlist has to be set to the ID as well for looping to work
+			playlist: ytID,
+		},
+		events: {
+			"onReady": onPlayerReady
+		}
+	})
+}
+
+const onPlayerReady = () => {
 	setTimeout(() => {
 		window.location.reload()
 	}, ytReload)
+}
+
+// Initiate the iframe player API, which gets picked up on
+// onYouTubePlayerAPIReady
+const ytMode = () => {
+	const script = document.createElement("script")
+	script.src = "https://www.youtube.com/player_api"
+	document.body.appendChild(script)
 }
 
 if (mode == "cycle")
