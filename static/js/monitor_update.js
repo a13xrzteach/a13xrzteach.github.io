3e989907de2ee392a5f45dab838edbe364337990
Michael Skyba <michaelskyba@net-c.com>
2024-05-08 13:23:38 -0400

    Show current settings

--- static/js/monitor_update.js
+++ static/js/monitor_update.js
@@ -21,3 +21,51 @@ sectionType.onchange = event => {
 		);
 	});
 };
+
+const parseCurrent = (section, config) => {
+	const sectionEl = document.getElementById(`current-${section}`);
+
+	const typeEl = document.createElement("li");
+	typeEl.innerHTML = `Section type: ${config.type}`;
+	sectionEl.appendChild(typeEl);
+
+	if (config.type == "image_cycle") {
+		const intervalEl = document.createElement("li");
+		intervalEl.innerHTML = `Cycle image switch interval (seconds): ${config.image_interval}`;
+		sectionEl.appendChild(intervalEl);
+
+		config.images.forEach(image => {
+			const imageLi = document.createElement("li");
+
+			const a = document.createElement("a");
+			a.href = `/static/img/monitor/${image}`;
+			a.innerHTML = image;
+
+			imageLi.appendChild(a);
+			sectionEl.appendChild(imageLi);
+		});
+	}
+
+	else if (config.type == "youtube") {
+		const linkEl = document.createElement("li");
+
+		const a = document.createElement("a");
+		const url = `https://youtube.com/watch?v=${config.video_id}`;
+		a.href = url;
+		a.innerHTML = url;
+
+		linkEl.appendChild(a);
+		sectionEl.appendChild(linkEl);
+	}
+}
+
+async function initCurrent() {
+	const raw = await fetch("/config");
+	const globalConfig = await raw.json();
+
+	["main", "footer", "sidebar"].forEach(
+		section => parseCurrent(section, globalConfig[section])
+	);
+}
+
+initCurrent();
