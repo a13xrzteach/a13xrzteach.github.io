e59eab3bdcf9b568933362761751bf77f94ddd82
Michael Skyba <michaelskyba@net-c.com>
2024-05-08 10:25:06 -0400

    Basic form input validation

--- monitor.py
+++ monitor.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+
+from typing import List, Optional
 
 app = FastAPI()
 
@@ -23,9 +25,41 @@ async def monitor_update():
     return FileResponse("monitor_update.html")
 
 
+def parse_youtube_id(youtube_url):
+    return "o-YBDTqX_ZU"
+
+
 @app.post("/api/update")
-async def monitor_api_update():
-    return "ok"
+async def monitor_api_update(
+    section: str = Form(...),
+    type: str = Form(...),
+    image_files: List[UploadFile] = File(...),
+    youtube_url: Optional[str] = Form(None),
+):
+    if section not in ["a", "b", "c"]:
+        return "Invalid section"
+
+    if type not in ["image_cycle", "youtube"]:
+        return "Invalid section type"
+
+    if type == "image_cycle":
+        for file in image_files:
+            if not file.filename:
+                return "image cycle type requires at least one image file"
+
+            if not file.content_type.startswith("image/"):
+                return "image cycle type only accepts image files"
+
+        return "ok image_cycle"
+
+    if not youtube_url:
+        return "No YouTube URL provided"
+
+    id = parse_youtube_id(youtube_url)
+    if not id:
+        return "Invalid YouTube URL format provided"
+
+    return "ok youtube"
 
 
 @app.get("/config")
