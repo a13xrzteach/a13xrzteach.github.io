a1b1f8f6e3ab95053a3123d3f3ad4887763456ed
Michael Skyba <michaelskyba@net-c.com>
2024-05-08 11:30:33 -0400

    Frontend validation

--- static/js/monitor_update.js
+++ static/js/monitor_update.js
@@ -1,14 +1,20 @@
 const sectionType = document.getElementById("type");
 
-const sectionsArray = ["youtube", "image_cycle"];
+const sectionsArray = ["image_cycle", "youtube"];
 const sections = {};
 sectionsArray.forEach(section => {
 	sections[section] = document.getElementById(section);
 });
 
+const inputRequirements = {
+	"image_cycle": document.getElementById("image_files"),
+	"youtube": document.getElementById("youtube_url"),
+};
+
 sectionType.onchange = event => {
 	sectionsArray.forEach(section => {
-		const display = section == event.target.value ? "block" : "none";
-		sections[section].style.display = display;
+		const isSelected = event.target.value == section;
+		sections[section].style.display = isSelected ? "block" : "none";
+		inputRequirements[section].required = isSelected;
 	});
-}
+};
