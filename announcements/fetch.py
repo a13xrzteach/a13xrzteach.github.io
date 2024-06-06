48257bb44dedfa74cdc795ae3f5aa8a60d4e1c2c
Michael Skyba <michaelskyba@net-c.com>
2024-06-06 12:35:27 -0400

    fetch.py: POC for getting title of announcements doc

---
+++ announcements/fetch.py
@@ -0,0 +1,34 @@
+# https://developers.google.com/docs/api/quickstart/python
+
+# GCP Project name: APAlpha
+# GCP Project number: 120642416673
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+SCOPES = ["https://www.googleapis.com/auth/documents.readonly"]
+ANNOUNCEMENTS_DOC_ID = "15bhSjMZYsuS6XkGMgsXq2I5qZchHRfrtQEQ_X6kqYrg"
+
+
+creds = None
+if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+        creds = flow.run_local_server(port=0)
+
+    with open("token.json", "w") as token:
+        token.write(creds.to_json())
+
+service = build("docs", "v1", credentials=creds)
+document = service.documents().get(documentId=ANNOUNCEMENTS_DOC_ID).execute()
+
+print(document.get("title")
